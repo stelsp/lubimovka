0c295ab23269a37ad29773415cfd204376a280cd
slava-k <stelspeh@gmail.com>
2021-09-27 00:19:31 +0300

    fix

--- scripts/performances.js
+++ scripts/performances.js
@@ -1,6 +1,8 @@
 const perf = document.querySelector('.performances');
 const perfList = document.querySelector('.performances__list');
 const perfTitle = document.querySelector('.performances__title');
+const perfItems = Array.from(document.querySelectorAll('.performances__item'));
+
 
   if (perfList.children.length === 1) {
     perfList.classList.add('performances__list_items_one');
@@ -14,6 +16,7 @@ const perfTitle = document.querySelector('.performances__title');
 
   if (perfList.children.length >= 3) {
     perfList.classList.add('performances__list_items_three-or-more');
+    perfItems.forEach(item => item.classList.add('performances__item_items_three-or-more'));
   }
 
 

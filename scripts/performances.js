99d11c9818d7b139190a456041f99827ce080464
slava-k <stelspeh@gmail.com>
2021-09-26 23:47:03 +0300

    fix

--- scripts/performances.js
+++ scripts/performances.js
@@ -1,8 +1,10 @@
 const perf = document.querySelector('.performances');
 const perfList = document.querySelector('.performances__list');
+const perfTitle = document.querySelector('.performances__title');
 
   if (perfList.children.length === 1) {
     perfList.classList.add('performances__list_items_one');
+    perfTitle.classList.add('performances__title_items_one')
     perf.classList.add('performances_items_one');
   }
 
